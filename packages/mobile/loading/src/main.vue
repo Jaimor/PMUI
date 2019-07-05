96ac7eddf3b207695e064fa4760e0fd814c0f50a
Jaimor Ye <jaimor.ye@utstarcom.cn>
2019-07-06 00:52:40 +0800

    完成loading

---
+++ packages/mobile/loading/src/main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="['pm-loading', { 'pm-loading--vertical': vertical }]">
+    <i :class="`pm-loading__icon pm-font pm-icon-${icon}`"></i>
+    <span :class="`pm-loading__text`">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "PmLoading",
+
+    props: {
+      icon: {
+        default: 'loading',
+        type: String
+      },
+      iconSize: String,
+      textSize: String,
+      vertical: Boolean
+    },
+    mounted() {
+      if (this.iconSize) this.$el.children[0].style.fontSize = this.iconSize;
+      if (this.textSize) this.$el.children[1].style.fontSize = this.textSize;
+    }
+  }
+</script>
---
+++ src/components/mobile/LoadingTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <pm-loading class="box" icon="loading1" icon-size="2rem"></pm-loading>
+    <pm-loading class="box">加载中...</pm-loading>
+    <pm-loading class="box" vertical>加载中...</pm-loading>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LoadingTest"
+  }
+</script>
+
+<style scoped>
+  .box {
+    margin-bottom: 20px;
+  }
+</style>
