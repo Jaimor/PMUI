c667c24c23c38d4e5b7a79245b9999d90a132c9d
Jaimor Ye <jaimor.ye@utstarcom.cn>
2019-07-08 13:24:37 +0800

    完成cell

--- src/components/mobile/CellTest.vue
+++ src/components/mobile/CellTest.vue
@@ -5,6 +5,14 @@
       <pm-cell icon="plus" title="点击测试" value="点我点我" @click="click1" is-link></pm-cell>
       <pm-cell icon="location-o" title="测试放大范德萨放大发" :value="2" :title-ratio="5" @click="click2"></pm-cell>
     </pm-cell-group>
+    <pm-cell-group title="测试2">
+      <pm-cell title="年龄" value="20"></pm-cell>
+      <pm-cell title="性别" value="男" :title-ratio="5"></pm-cell>
+    </pm-cell-group>
+    <pm-cell-group title="测试3">
+      <pm-cell title="商品" value="黄瓜"></pm-cell>
+      <pm-cell title="单价" value="￥10" :title-ratio="5"></pm-cell>
+    </pm-cell-group>
   </div>
 </template>
 
