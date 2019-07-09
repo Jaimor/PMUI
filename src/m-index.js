adc0da2844491ded4649cfff0d998b1cb83f09fc
jaimor <1239666205@qq.com>
2019-07-10 07:44:08 +0800

    修改组件路径中Icon为icon

--- src/m-index.js
+++ src/m-index.js
@@ -2,7 +2,7 @@ import Button from '../packages/mobile/button/index.js';
 import Cell from '../packages/mobile/cell/index.js';
 import CellGroup from '../packages/mobile/cell-group/index.js';
 import Col from '../packages/mobile/col/index.js';
-import Icon from '../packages/mobile/Icon/index.js';
+import Icon from '../packages/mobile/icon/index.js';
 import Loading from '../packages/mobile/loading/index.js';
 import Popup from '../packages/mobile/popup/index.js';
 import Row from '../packages/mobile/row/index.js';
