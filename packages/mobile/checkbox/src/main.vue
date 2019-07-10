a4a54a88c99c98d652c583afceecfeede9fb03c6
jaimor <1239666205@qq.com>
2019-07-10 22:55:48 +0800

    完成checkbox组件

--- packages/mobile/checkbox/src/main.vue
+++ packages/mobile/checkbox/src/main.vue
@@ -3,27 +3,29 @@
     :class="['pm-checkbox']"
   >
     <div
-      class="pm-checkbox__icon"
-      :style="{backgroundColor: iconBackgroundColor}"
+      :class="['pm-checkbox__icon', { 'pm-checkbox__label--disabled': disabled }]"
+      :style="{backgroundColor: iconBackgroundColor, borderColor: iconBorderColor}"
       @click="iconClickHandler"
     >
       <pm-icon v-if="value" icon="success" color="white" font-size=".8rem"></pm-icon>
     </div>
-    <div
-      class="pm-checkbox__label"
+    <label
+      :class="['pm-checkbox__label', { 'pm-checkbox__label--disabled': disabled }]"
       @click="labelClickHandler"
     >
       <slot></slot>
-    </div>
+    </label>
   </div>
 </template>
 
 <script>
+  import Constant from './constant.js';
   export default {
     name: "PmCheckbox",
     data() {
       return {
-        iconBackgroundColor: 'white'
+        iconBackgroundColor: this.value ? this.checkedColor : Constant.checkbox.iconBackgroundColorDefault,
+        iconBorderColor: this.value ? this.checkedColor : Constant.checkbox.iconBorderColorDefault
       }
     },
     props: {
@@ -33,21 +35,30 @@
       },
       checkedColor: {
         type: String,
-        default: '#1989fa'
-      }
+        default: Constant.checkbox.iconCheckedColorDefault
+      },
+      disabled: Boolean
     },
     watch: {
       value(v) {
         if (v) {
           this.iconBackgroundColor = this.checkedColor;
+          this.iconBorderColor = this.checkedColor;
         } else {
-          this.iconBackgroundColor = 'white';
+          this.iconBackgroundColor = Constant.checkbox.iconBackgroundColorDefault;
+          this.iconBorderColor = Constant.checkbox.iconBorderColorDefault;
         }
+      }
+    },
+    created() {
+      if (this.disabled) {
+        this.iconBackgroundColor = Constant.checkbox.iconBorderColorDefault;
+        this.iconBorderColor = Constant.checkbox.iconDisabledBorderColor
       }
     },
     methods: {
       toggle() {
-        this.$emit('input', !this.value);
+        if (!this.disabled) this.$emit('input', !this.value);
       },
       iconClickHandler() {
         this.toggle();
--- src/components/mobile/CheckboxTest.vue
+++ src/components/mobile/CheckboxTest.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <pm-checkbox v-model="check">xxx</pm-checkbox>
+    <div style="margin-bottom: 10px;">
+      <p>基本用法</p>
+      <pm-checkbox v-model="check1">这里是基本用法</pm-checkbox>
+    </div>
+    <div style="margin-bottom: 10px;">
+      <p>禁用状态</p>
+      <pm-checkbox v-model="check2" disabled>未选中禁用</pm-checkbox>
+      <pm-checkbox v-model="check3" :disabled="true">选中禁用</pm-checkbox>
+    </div>
+    <div style="margin-bottom: 10px;">
+      <p>自定义颜色</p>
+      <pm-checkbox v-model="check4" checked-color="rgb(7, 193, 96)">自定义颜色</pm-checkbox>
+    </div>
   </div>
 </template>
 
@@ -9,7 +21,10 @@
     name: "CheckboxTest",
     data() {
       return {
-        check: false
+        check1: false,
+        check2: false,
+        check3: true,
+        check4: false
       }
     }
   }
