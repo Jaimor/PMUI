49da64ed381d8d5482e28a97391269587de6632f
Jaimor Ye <jaimor.ye@utstarcom.cn>
2019-07-15 18:16:21 +0800

    正在编写picker组件

--- packages/mobile/picker/src/constant.js
+++ packages/mobile/picker/src/constant.js
@@ -1,4 +1,11 @@
 export default {
   ulKey: 'PM_PICKER_UL',
-  liKey: 'PM_PICKER_LI'
+  liKey: 'PM_PICKER_LI',
+  util: {
+    getParent(event) {
+      if (event.target)
+        return event.target.parentNode;
+      return null;
+    }
+  }
 }
--- packages/mobile/picker/src/main.vue
+++ packages/mobile/picker/src/main.vue
@@ -6,12 +6,26 @@
       <span class="pm-picker__header__done">完成</span>
     </div>
     <div class="pm-picker__content">
-      <div class="pm-picker__content--first"></div>
-      <div class="pm-picker__content--selected"></div>
-      <div class="pm-picker__content--bottom"></div>
-      <ul v-for="(col, index) in columns" :key="`${ulKey}_${index}`" :style="{width: `${100/columns.length}%`}">
-        <li v-for="(v, i) in col.values" :key="`${liKey}_${i}`"> {{v}} </li>
-      </ul>
+      <div
+        class="pm-picker__content--touch-event"
+        v-for="(col, index) in columns" :key="`${ulKey}_${index}`"
+        :style="{width: `${100/columns.length}%`}"
+        @touchstart="touchStart"
+        @touchmove="touchMove"
+        @touchend="touchEnd"
+      >
+        <div class="pm-picker__content--top"></div>
+        <div class="pm-picker__content--selected"></div>
+        <div class="pm-picker__content--bottom"></div>
+        <ul
+          :style="{top: top}"
+        >
+          <li
+            v-for="(v, i) in col.values"
+            :key="`${liKey}_${i}`"
+          > {{v}} </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +37,9 @@
     data() {
       return {
         ulKey: Constant.ulKey,
-        liKey: Constant.liKey
+        liKey: Constant.liKey,
+        clientY: 0,
+        top: 0
       }
     },
     props: {
@@ -31,6 +47,25 @@
       columns: {
         type: Array,
         required: true
+      }
+    },
+    methods: {
+      touchStart(e) {
+        e.preventDefault();
+        this.clientY = e.targetTouches[0].clientY;
+      },
+      touchMove(e) {
+        e.preventDefault();
+        const ul = Constant.util.getParent(e).children[3];
+        this.top += e.targetTouches[0].clientY - this.clientY;
+        console.log(this.top)
+        ul.style.top = this.top + 'px';
+        // this.$nextTick(_ => {
+        //   ul.style.top = top + 'px';
+        // })
+      },
+      touchEnd(e) {
+        console.log("end")
       }
     }
   }
