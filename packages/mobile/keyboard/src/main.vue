d9e352d28a3b5592aaec9239809d7621aaf76127
jaimor <1239666205@qq.com>
2019-07-18 23:13:55 +0800

    keyboard完成

--- packages/mobile/keyboard/src/main.vue
+++ packages/mobile/keyboard/src/main.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="pm-keyboard">
-
+    <div class="pm-keyboard__number">
+      <span @click="numberClickHandler(1)">1</span><span @click="numberClickHandler(2)">2</span><span @click="numberClickHandler(3)">3</span>
+      <span @click="numberClickHandler(4)">4</span><span @click="numberClickHandler(5)">5</span><span @click="numberClickHandler(6)">6</span>
+      <span @click="numberClickHandler(7)">7</span><span @click="numberClickHandler(8)">8</span><span @click="numberClickHandler(9)">9</span>
+      <span @click="numberClickHandler(0)">0</span><span @click="numberClickHandler('.')">.</span>
+    </div>
+    <div class="pm-keyboard__btn">
+      <span @click="del">删除</span>
+      <span @click="done">完成</span>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "PmKeyboard"
+    name: "PmKeyboard",
+    methods: {
+      numberClickHandler(key) {
+        this.$emit("number-click", key);
+      },
+      del() {
+        this.$emit('delete');
+      },
+      done() {
+        this.$emit('done');
+      }
+    }
   }
 </script>
--- src/components/mobile/KeyboardTest.vue
+++ src/components/mobile/KeyboardTest.vue
@@ -1,10 +1,42 @@
 <template>
-  <div></div>
+  <div>
+    <pm-navbar title="Keyboard" left-arrow back></pm-navbar>
+    <p style="text-align: left;padding-left: 10px;">
+      <pm-button type="primary" @click="openClick">打开键盘</pm-button>
+    </p>
+    <pm-popup position="bottom" v-model="open" :show-mask="false">
+      <pm-keyboard
+        @number-click="numClick"
+        @delete="del"
+        @done="done"
+      ></pm-keyboard>
+    </pm-popup>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "KeyboardTest"
+    name: "KeyboardTest",
+    data() {
+      return {
+        open: false
+      }
+    },
+    methods: {
+      openClick() {
+        this.open = true;
+      },
+      numClick(key) {
+        this.$toast.info("点击 " + key);
+      },
+      del() {
+        this.$toast.failed("删除按钮")
+      },
+      done() {
+        this.$toast.success("完成按钮")
+        this.open = false;
+      }
+    }
   }
 </script>
 
